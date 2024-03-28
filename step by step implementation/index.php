f777773f35a54573f568d51cb18a859e1a3872a6
Borgs <nisiyawa55@gmail.com>
2024-03-28 00:41:37 -0700

    Create html tags with Emmet
    Started with creating the HTML skeleton of the project
    by using emmet to avoid the copy paste dillema. Since there is a
    need to document step by step process- a slight deviation from the
    original process helps me familiarize with the code.
    
    Downloaded the MySQL script file to be executed in preparation for
    future database operations.

--- step by step implementation/index.php
+++ step by step implementation/index.php
@@ -11,6 +11,71 @@
 </head>
 <body>
 
+  <!-- div.container>div[class="card mt-3"]>div.card-header>h5{Employee Table} -->
+  <div class="container">
+    <div class="card mt-3">
+      <div class="card-header">
+        <h5>Employee Table</h5>
+      </div>
+
+      <!-- div.card-body>a[href="3" class="btn btn-primary btn-sm" id="btnAdd" ]{+ Add employee} -->
+      <div class="card-body">
+        <a href="3" class="btn btn-primary btn-sm" id="btnAdd">+ Add employee</a>
+        <!-- table[id="table1" class="table table-bordered"]>thead>tr>th{Employee}+th{Name}+th{Email}+th -->
+        <table id="table1" class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Employee</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <!-- tbody>td{ID Data}+td{Name Data}+td{Email Data}+td -->
+          <tbody>
+            <td>ID Data</td>
+            <td>Name Data</td>
+            <td>Email Data</td>
+            <td>
+
+              <!-- a[href="#" id="btnEdit" data-id="to be filled" data-first_name="to be filled" data-last_name="to be filled" data-email="to be filled" class="btn btn-warning btn-sm"]{EDIT} -->
+              <a 
+                href="#" 
+                id="btnEdit" 
+                data-id="to be filled" 
+                data-first_name="to be filled" 
+                data-last_name="to be filled" 
+                data-email="to be filled" 
+                class="btn btn-warning btn-sm">
+                EDIT
+              </a>
+
+              <!-- a[class="btn btn-danger btn-sm" href="#" id="btnDelete" data-id="to be filled"] -->
+              <a href="#" class="btn btn-danger btn-sm" id="btnDelete" data-id="to be filled">
+                DELETE
+              </a>
+
+              
+
+            </td>
+          </tbody>
+
+          
+        </table
+
+      </div>
+      
+
+
+    </div>
+  </div>
+  
+
+
+
+
+
+
 <!-- import scripts -->
 <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="sweetalert/sweetalert2.all.min.js"></script>
@@ -19,7 +84,7 @@
   
 <!-- event handling code -->
 <script>
-  
+
 </script>
 </body>
 </html>
