36bf212a3d062430559d5b39780b4759a78bd253
Borgs <nisiyawa55@gmail.com>
2024-03-27 23:42:43 -0700

    base file for index page & dependecy set-up
    prepate the link and script tag for the index page.

---
+++ step by step implementation/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD Step-by-Step</title>
+
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="sweetalert/sweetalert2.min.css">
+  <link rel="stylesheet" href="datatables/datatables.min.css">
+</head>
+<body>
+
+<!-- import scripts -->
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="sweetalert/sweetalert2.all.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="datatables/datatables.min.js"></script>
+  
+<!-- event handling code -->
+<script>
+  
+</script>
+</body>
+</html>
