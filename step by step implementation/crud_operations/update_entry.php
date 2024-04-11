19aed4200ce02f2697eefe264239705bda945b86
Borgs <nisiyawa55@gmail.com>
2024-04-10 23:42:27 -0700

    Create php script for Update Process
    For every table that updates an entry, the
    php file that will be called should be this file.
    
    This will likely break. to be debugged.
    
    The connection used by the update is a connection
    from the session.
    
    I want to use the session to
    store a single connection that executes mysql queries
    rather than create a new mysql connection
    for every query.

---
+++ step by step implementation/crud_operations/update_entry.php
@@ -0,0 +1,46 @@
+<?php
+require_once('dbOperations.php');
+require_once('response.php');
+
+$conn = getHRDBConnection();
+
+if( !$_SERVER['REQUEST_METHOD'] == 'POST' ) {
+  return;
+}
+
+
+$update_values = "";
+
+$columnCount = 0;
+$data = $_POST['data'];
+
+foreach ($data as $key => $value) {
+  $update_values .= $key . "='". $value . "',";
+}
+
+$regPattern = '/,(?=[^,]*$)/';
+$update_values = preg_replace( $regPattern, "",$update_values);
+
+
+$sqlQuery = "UPDATE " . $_POST['table_name'] . " SET " . $update_values . ";";
+
+
+
+if (session_status() == PHP_SESSION_NONE) { session_start(); }
+
+if(!isset($_SESSION['hr_db_conn'])){
+  $_SESSION['hr_db_conn'] = getHRDBConnection();
+}
+
+$result = $_SESSION['hr_db_conn']->query($sqlQuery);
+
+$response = ($result) ?
+[
+  "status" => "success"
+] :
+[
+  "errorMessage" => "Error Number: " . $conn->connect_errno . '\n' . $conn->error . '\n' . $sqlQuery,
+  "status" => "error"
+];
+
+echo json_encode($response);
