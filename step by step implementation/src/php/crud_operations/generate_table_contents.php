a01404088aae60810ed9f7b8cbe82b5fb56404e7
Borgs <nisiyawa55@gmail.com>
2024-04-16 21:07:52 -0700

    Delegated code portions to Separate Functions
    I find it hard to read code that creates
    a string representation of html code.
    
    I kind of made a mistake in handling html creation with php code.
    
    I should have made this portion in javascript and client side, so that
    data sent between server and client is minimal.

--- step by step implementation/src/php/crud_operations/generate_table_contents.php
+++ step by step implementation/src/php/crud_operations/generate_table_contents.php
@@ -2,21 +2,26 @@
 include_once "dbOperations.php";
 include_once("table_data.php");
 
-function generate_add_entry_details($tableName) {
+function create_options_element($value,$display)
+{
+  $option_template = '<option value="%s">%s</option>';
+  return sprintf( $option_template, $value, $display);
+}
+function generate_add_entry_details($tableName) 
+{
 
   if(session_status() == PHP_SESSION_NONE){
     session_start();
   }
 
   if (!isset($_SESSION['table_with_foreign_data'])) {
-    // terminate function, might cause an error
+    // terminate function, might cause an error, data about tables with foreign data must be available before function execution 
     return;
   }
 
 
   $column_with_sqlquery = $_SESSION['table_with_foreign_data'][$tableName];
 
-  $option_template = '<option value="%s">%s</option>';
   $response = [];
 
   foreach( $column_with_sqlquery as $key => $value ){
@@ -29,10 +34,11 @@ function generate_add_entry_details($tableName) {
     }
 
     $options_html_form = "";
+    
     $row_entry = mysqli_fetch_assoc($result['result']); 
     
     while( $row_entry != null ){
-      $options_html_form .= sprintf($option_template, $row_entry["value_column"],$row_entry["display_column"]);
+      $options_html_form .= create_options_element($row_entry["value_column"],$row_entry["display_column"]);
       $row_entry = mysqli_fetch_assoc($result['result']); 
     }
     $response[$key] = $options_html_form; 
@@ -41,58 +47,53 @@ function generate_add_entry_details($tableName) {
   return $response;
 }
 
-function generate_body_content($table_data_raw, $primary_key_id) {
+function generate_table_row(string $primary_key_id, array $table_row_data, string $primary_value)
+{
   $table_row_template = "<tr>
             <td>
               <button
                 data-primary-key=\"$primary_key_id\"
                 data-primary-value=\"%s\"
                 class=\"btn btn-warning btn-sm btnEdit\"
-                type=\"button\"
-                >
+                type=\"button\" >
                 EDIT
               </button>
               <button 
                 class=\"btn btn-danger btn-sm btnDelete\" 
                 type=\"button\"
                 data-primary-key=\"$primary_key_id\"
-                data-primary-value=\"%s\"
-                >
+                data-primary-value=\"%s\" >
                 DELETE
               </button>
             </td>
             %s
           </tr>";
-
   $table_data_template = "<td>%s</td>\n";
 
+  $row_table_data_html = "";
+  foreach($table_row_data as $table_data){
+    $row_table_data_html .= sprintf($table_data_template, $table_data);
+  }
+
+  return sprintf($table_row_template,$primary_value,$primary_value ,$row_table_data_html );
+}
+
+function generate_body_content( mysqli_result $table_data_raw, string $primary_key_id) 
+{
+
   $table_Body_HTML = "";
-  $row_table_data = "";
-
-  $filtered_row_data = [];
-  $all_filtered_row_data = [];
-  
   while ($row = $table_data_raw->fetch_row()) { 
-
-    foreach($row as $table_data){
-      $row_table_data .= sprintf($table_data_template, $table_data);
-      $filtered_row_data[] = $table_data;
-    }
-
-    $table_Body_HTML .= sprintf($table_row_template, $row[0],$row[0], $row_table_data);
-    $all_filtered_row_data[$row[0]] = $filtered_row_data; 
-
-    $row_table_data = "";
-    $filtered_row_data = [];
+    $table_Body_HTML .= generate_table_row($primary_key_id, $row , $row[0] );
   }
 
   return [
     'body'=>$table_Body_HTML,
-    'filtered_data'=>$all_filtered_row_data
   ];
 }
 
-function generate_column_headers($column_headers) {
+
+function generate_column_headers($column_headers)
+{
 
   $header_row_template= "\n<tr>
     <th>\n
@@ -122,7 +123,8 @@ function generate_column_headers($column_headers) {
   ];
 }
 
-function generate_table($tableName){
+function generate_table($tableName)
+{
 
   if(session_status() == PHP_SESSION_NONE ){
     session_start();
@@ -144,14 +146,17 @@ function generate_table($tableName){
     return;
   }
 
-  $column_headers = $result["result"]->fetch_fields();  
-  $resultHeader = generate_column_headers($column_headers);
-  $resultBody = generate_body_content($result["result"], $column_headers[0]->name);
+  $column_headers = $result["result"]->fetch_fields();
+  $primary_key = $column_headers[0]->name;
+
   
-  $columnOptionsForForeignData = 
-  array_key_exists($tableName,$_SESSION['table_with_foreign_data']) && isset($_SESSION['table_with_foreign_data']) ? 
-  generate_add_entry_details($tableName) :
-  [""] ;
+  $resultHeader = generate_column_headers($column_headers);
+  
+  $resultBody = generate_body_content($result["result"], $primary_key);
+
+  
+  $is_foreign_data_necessary = array_key_exists($tableName, $_SESSION['table_with_foreign_data']);
+  $columnOptionsForForeignData = $is_foreign_data_necessary ? generate_add_entry_details($tableName) : "";
 
   return [
     "table_name" => $tableName,
@@ -163,9 +168,11 @@ function generate_table($tableName){
 }
 
 
-if (isset($_POST['input-table-name']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+if (isset($_POST['input-table-name']) && $_SERVER['REQUEST_METHOD'] == 'POST') 
+{
   $tableData = generate_table($_POST['input-table-name']);
-  echo json_encode($tableData);
+    $response = json_encode($tableData);
+  echo $response;
 }
 
 
